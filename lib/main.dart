88468541c51e062dad94b9f8cbc7ad22e3ebbdde
kimsungkyum <sungkyum1@naver.com>
2022-11-04 01:48:32 +0900

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -73,6 +73,91 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: Column(children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color(0xffffc105),
+            ),
+            child: Container(
+              width: double.infinity,
+              child: Column(children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 40,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Image.network(
+                        "https://i.ibb.co/CwzHq4z/trans-logo-512.png"),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '닉네임',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                      fontSize: 20),
+                ),
+                Text(
+                  'hello@world.com',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                      fontSize: 18),
+                ),
+              ]),
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 3 / 1,
+            child: PageView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Image.network(
+                    "https://i.ibb.co/Q97cmkg/sale-event-banner1.jpg"),
+                Image.network(
+                    "https://i.ibb.co/GV78j68/sale-event-banner2.jpg"),
+                Image.network(
+                    "https://i.ibb.co/R3P3RHw/sale-event-banner3.jpg"),
+                Image.network(
+                    "https://i.ibb.co/LRb1VYs/sale-event-banner4.jpg"),
+              ],
+            ),
+          ),
+          ListTile(
+            title: Text(
+              '구매내역',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black),
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              size: 20,
+              color: Colors.black,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              '저장한 레시피',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black),
+            ),
+            trailing: Icon(
+              Icons.arrow_forward_ios,
+              size: 20,
+              color: Colors.black,
+            ),
+          ),
+        ]),
+      ),
       body: Column(
         children: [
           Padding(
